788d8f9ab3cb4f408411144659eaf819ff38dfe2
KM-Tanvir-Anjum <88136108+KM-Tanvir-Anjum@users.noreply.github.com>
2025-05-16 15:40:00 +0600

    Update pen.py

--- pen.py
+++ pen.py
@@ -1,5 +1,5 @@
 from icrawler.builtin import BingImageCrawler  # instead of GoogleImageCrawler
 
 search_term = input("Enter the object you want to download images of: ")
-bing_crawler = BingImageCrawler(storage={'root_dir': f'images/shahioal'})
+bing_crawler = BingImageCrawler(storage={'root_dir': f'images/pen'})
 bing_crawler.crawl(keyword=search_term, max_num=30)
