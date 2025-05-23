4a684f7157e34bcca0e6ef5080aad6351d84fa20
KM-Tanvir-Anjum <88136108+KM-Tanvir-Anjum@users.noreply.github.com>
2025-05-23 09:25:11 +0600

    Update pen.py

--- pen.py
+++ pen.py
@@ -1,5 +1,11 @@
-from icrawler.builtin import BingImageCrawler  # instead of GoogleImageCrawler
+from icrawler.builtin import BingImageCrawler
 
-search_term = input("Enter the object you want to download images of: ")
-bing_crawler = BingImageCrawler(storage={'root_dir': f'images/pen'})
+# Define a more specific keyword to avoid irrelevant results (like Apple logo, products, etc.)
+search_term = "apple fruit"
+
+# Create a safe folder name
+safe_search_term = search_term.replace(" ", "_")
+
+# Set up the crawler
+bing_crawler = BingImageCrawler(storage={'root_dir': f'images/{safe_search_term}'})
 bing_crawler.crawl(keyword=search_term, max_num=30)
